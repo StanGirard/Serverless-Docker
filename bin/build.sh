1209764665989d444a6eb3e2880f8a908fc502f1
Stanislas Girard <stan@primates.dev>
2020-11-26 18:52:56 +0100

    ci: 🎡 Version File

--- bin/build.sh
+++ bin/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e 
+export RELEASE=`cat VERSION`
 if [[ -z $RELEASE ]]; then
     echo "Error: undefined RELEASE environment variable."
     exit 1
 fi
+
 docker build --build-arg RELEASE="$RELEASE" -t stangirard/serverless .
 docker tag stangirard/serverless:latest stangirard/serverless:"$RELEASE"
--- bin/push.sh
+++ bin/push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e 
+export RELEASE=`cat VERSION`
 echo "$DOCKER_PASS" | docker login --username "$DOCKER_USER" --password-stdin
 docker push stangirard/serverless:latest;
 docker push stangirard/serverless:"$RELEASE";
